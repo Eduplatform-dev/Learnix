d2af30e4b826ac9ae048cdefc656b1092cf78864
Rushi-5320 <edwithru@gmail.com>
2026-03-21 03:54:24 +0530

    dependancies clear

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,7 @@ export default defineConfig({
     },
   },
   test: {
-    globals: true,
-    environment: 'jsdom',
+    globals: true,nvironment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
   },
 })
